dbcc40fa4f95f77ccedbc77cbf5ed44fc75458d0
Umar Wahid <66332510+umarwahid@users.noreply.github.com>
2023-07-26 09:34:00 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -36,7 +36,7 @@ app.get("/products", async (req, res) => {
   const productsCollection = database.collection('products');
 
   const query = {}
-  const findproducts = await productsCollection.findOne(query);
+  const findproducts = await productsCollection.find(query);
   await client.close();
 
   res.send({
